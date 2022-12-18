cd18a804331910923c4e566efc3dfc74e24fa647
Ny Andry <miantsa.andr@gmail.com>
2022-12-18 17:04:00 +0300

    Finished refactoring 4.2

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
+const app = require("./app");
 const http = require("http");
 const config = require("./utils/config");
-const blogsRouter = require('./controllers/blogs')
 const logger = require("./utils/logger");
-const express = require("express");
-const Blog = require("./models/blog");
-const app = express();
-const cors = require("cors");
-
-const mongoose = require("mongoose");
-mongoose.connect(config.MONGODB_URI);
-
-app.use(cors());
-app.use(express.json());
-
-app.use('/api/blogs', blogsRouter)
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
