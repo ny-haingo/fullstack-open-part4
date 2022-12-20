2b6eccbbd5e994a7ad4c5c69fd0b0b707efbc22a
Ny Andry <miantsa.andr@gmail.com>
2022-12-20 19:08:28 +0300

    4.5

---
+++ tests/list_helper.test.js
@@ -0,0 +1,41 @@
+const { dummy, totalLikes, favoriteBlog } = require("../utils/list_helper");
+
+const testBlogs = require("../utils/testBlogs");
+
+const listWithOneBlog = [testBlogs[0]];
+
+test("dummy returns one", () => {
+  const blogs = [];
+
+  const result = dummy(blogs);
+  expect(result).toBe(1);
+});
+
+describe("total likes", () => {
+  test("an empty list is zero", () => {
+    const result = totalLikes([]);
+    expect(result).toBe(0);
+  });
+
+  test("when list has only one blog, equals the like of that", () => {
+    const result = totalLikes(listWithOneBlog);
+    expect(result).toBe(12);
+  });
+
+  test("of a bigger list is calculated right", () => {
+    const result = totalLikes(testBlogs);
+    expect(result).toBe(142);
+  });
+});
+
+describe("most likes", () => {
+  test("when one author has the most", () => {
+    const result = favoriteBlog(testBlogs);
+    const mostPopular = {
+      title: "The Selfish Gene",
+      author: "Richard Dawkins",
+      likes: 30,
+    };
+    expect(result).toEqual(mostPopular);
+  });
+});
---
+++ utils/list_helper.js
@@ -0,0 +1,19 @@
+const dummy = (blogs) => 1;
+
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => sum + item;
+
+  return blogs.map((blog) => blog.likes).reduce(reducer, 0);
+};
+
+const favoriteBlog = (blogs) => {
+  const favorite = blogs.sort((a, b) => b.likes - a.likes);
+
+  return favorite[0] || null;
+};
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+};
