7dc1d18e0dc7be078d9c5acb3a41fb1bae90022c
Ny Andry <miantsa.andr@gmail.com>
2022-12-20 21:13:26 +0300

    json test

---
+++ tests/blog_api.test.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+
+const api = supertest(app);
+
+describe("The right HTTP request", () => {
+  test("blogs are returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
