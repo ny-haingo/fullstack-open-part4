9591a177892d99299381d286bfc5872a36f38eb1
Ny Andry <miantsa.andr@gmail.com>
2022-12-21 01:39:34 +0300

    refactoring

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -8,15 +8,6 @@ const { info } = require("../utils/logger");
 
 const api = supertest(app);
 
-// beforeEach(async () => {
-//   await Blog.deleteMany({});
-
-//   for (let blog of testBlogs) {
-//     let blogObject = new Blog(blog);
-//     await blogObject.save();
-//   }
-// });
-
 beforeEach(async () => {
   await Blog.deleteMany({});
 
@@ -25,7 +16,7 @@ beforeEach(async () => {
   await Promise.all(promiseArray);
 });
 
-describe("The right HTTP request", () => {
+describe("get tests", () => {
   test("blogs are returned as json", async () => {
     await api
       .get("/api/blogs")
@@ -40,31 +31,35 @@ describe("The right HTTP request", () => {
   });
 });
 
-test("the unique identifier property of the blog posts is named id", () => {
-  const newBlog = new Blog();
-  expect(newBlog.id).toBeDefined();
+describe("post tests", () => {
+  test("a valid blog can be added", async () => {
+    const newBlog = {
+      title: "We need love",
+      author: "Miantsa",
+      url: "url",
+      like: 0,
+    };
+
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const response = await api.get("/api/blogs");
+
+    const titles = response.body.map((r) => r.title);
+
+    expect(response.body).toHaveLength(testBlogs.length + 1);
+    expect(titles).toContain("We need love");
+  });
 });
 
-test("a valid blog can be added", async () => {
-  const newBlog = {
-    title: "We need love",
-    author: "Miantsa",
-    url: "url",
-    like: 0,
-  };
-
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
-
-  const response = await api.get("/api/blogs");
-
-  const titles = response.body.map((r) => r.title);
-
-  expect(response.body).toHaveLength(testBlogs.length + 1);
-  expect(titles).toContain("We need love");
+describe("validity", () => {
+  test("the unique identifier property of the blog posts is named id", () => {
+    const newBlog = new Blog();
+    expect(newBlog.id).toBeDefined();
+  });
 });
 
 afterAll(() => {
