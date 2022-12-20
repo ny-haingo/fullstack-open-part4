6b4ee16cf383a3a35d6ad26a31b05d6592ac881c
Ny Andry <miantsa.andr@gmail.com>
2022-12-21 02:44:52 +0300

    ..

--- utils/testBlogs.js
+++ utils/testBlogs.js
@@ -1,44 +1,44 @@
 const blogs = [
-    {
-      title: "Canonical string reduction",
-      author: "Edsger W. Dijkstra",
-      likes: 12
-    },
-    {
-      title: "A Technique for Counting Ones in a Binary Computer",
-      author: "Edsger W. Dijkstra",
-      likes: 8
-    },
-    {
-      title: "On the role of scientific thought",
-      author: "Albert Einstein",
-      likes: 15
-    },
-    {
-      title: "The Nature of Space and Time",
-      author: "Stephen Hawking",
-      likes: 25
-    },
-    {
-      title: "The Road to Reality",
-      author: "Roger Penrose",
-      likes: 18
-    },
-    {
-      title: "Cognitive Psychology",
-      author: "David Groome",
-      likes: 22
-    },
-    {
-      title: "The Selfish Gene",
-      author: "Richard Dawkins",
-      likes: 30
-    },
-    {
-      title: "The Structure of Scientific Revolutions",
-      author: "Thomas S. Kuhn",
-      likes: 12
-    }
-  ];
-  
-  module.exports = blogs
+  {
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    likes: 12,
+  },
+  {
+    title: "A Technique for Counting Ones in a Binary Computer",
+    author: "Edsger W. Dijkstra",
+    likes: 8,
+  },
+  {
+    title: "On the role of scientific thought",
+    author: "Albert Einstein",
+    likes: 15,
+  },
+  {
+    title: "The Nature of Space and Time",
+    author: "Stephen Hawking",
+    likes: 25,
+  },
+  {
+    title: "The Road to Reality",
+    author: "Roger Penrose",
+    likes: 18,
+  },
+  {
+    title: "Cognitive Psychology",
+    author: "David Groome",
+    likes: 22,
+  },
+  {
+    title: "The Selfish Gene",
+    author: "Richard Dawkins",
+    likes: 30,
+  },
+  {
+    title: "The Structure of Scientific Revolutions",
+    author: "Thomas S. Kuhn",
+    likes: 12,
+  },
+];
+
+module.exports = blogs;
