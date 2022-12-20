8645f3f5ef4cca656cac1a7a14f3c2bf2a651bc6
Ny Andry <miantsa.andr@gmail.com>
2022-12-20 21:44:58 +0300

    refactor backend to use async/await

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,8 +1,19 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
+const testBlogs = require("../utils/testBlogs");
+const Blog = require("../models/blog");
 
 const api = supertest(app);
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+
+  for (let blog of testBlogs) {
+    let blogObject = new Blog(blog);
+    await blogObject.save();
+  }
+});
 
 describe("The right HTTP request", () => {
   test("blogs are returned as json", async () => {
