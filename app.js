d317e2ab9e980414c5b2978d01cbf240ad60a3f0
Ny Andry <miantsa.andr@gmail.com>
2022-12-22 14:10:10 +0300

    Adding user is now possible

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const config = require("./utils/config");
 const express = require("express");
 const app = express();
 const cors = require("cors");
+
+const usersRouter = require("./controllers/users");
 const blogsRouter = require("./controllers/blogs");
+
 const logger = require("./utils/logger");
 const mongoose = require("mongoose");
 
@@ -20,6 +23,7 @@ mongoose
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/users", usersRouter);
 app.use("/api/blogs", blogsRouter);
 
 module.exports = app;
